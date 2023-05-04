1b6c48cbfe2337a89bf97f3ce902eef4f49423c6
Sarim Abbas <sarim.stays@gmail.com>
2023-05-04 02:57:12 -0700

    wip

--- packages/web/src/app/api/v1/route.ts
+++ packages/web/src/app/api/v1/route.ts
@@ -4,8 +4,6 @@ import normalizeUrl from "normalize-url";
 
 const commonHeaders: Record<string, string> = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export async function POST(request: NextRequest) {
