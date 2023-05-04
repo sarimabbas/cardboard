ab9166ab15b65a9f01f5b018911a290d68f5767d
Sarim Abbas <sarim.stays@gmail.com>
2023-05-04 01:43:45 -0700

    wip

---
+++ packages/publish/src/index.ts
@@ -0,0 +1,1 @@
+export { Embed as Cardboard } from "@cardboard/shared";
---
+++ packages/publish/tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entryPoints: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  noExternal: ["@cardboard/shared"],
+});
