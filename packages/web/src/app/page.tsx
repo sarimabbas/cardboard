ad7d473af12d7b4035929de5a85fea2e95933cad
Sarim Abbas <sarim.stays@gmail.com>
2023-05-04 16:19:34 -0700

    wip

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [url, setUrl] = useState<string>("");
+  const [urls, setUrls] = useState<string[]>([]);
 
   return (
     <div className="flex flex-col justify-center gap-8">
@@ -14,8 +15,21 @@ export default function Home() {
         placeholder="Your URL here"
         onChange={(e) => setUrl(e.target.value)}
       />
+      <button
+        className="p-4 border rounded-md"
+        onClick={() => {
+          if (url === "") {
+            return;
+          }
+          setUrls([...urls, url]);
+        }}
+      >
+        Add
+      </button>
       <CardboardProvider>
-        <Cardboard url={url} providerService="/api/v1" />
+        {urls.map((url) => (
+          <Cardboard url={url} />
+        ))}
       </CardboardProvider>
     </div>
   );
