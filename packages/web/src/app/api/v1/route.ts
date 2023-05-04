ecdbf697b63a9a6a79a7cbac4b44e9e8d37cf46b
Sarim Abbas <sarim.stays@gmail.com>
2023-05-04 00:41:38 -0700

    wip

---
+++ packages/web/src/app/api/v1/route.ts
@@ -0,0 +1,38 @@
+import { OEmbedProvider } from "@/contract";
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const params = Object.fromEntries(url.searchParams);
+
+  // find the oembed provider
+  const providers = await fetch("https://oembed.com/providers.json");
+  const providersJson: OEmbedProvider[] = await providers.json();
+
+  // find the provider that matches the url
+  const provider = providersJson.find((provider) =>
+    params.url.includes(provider.provider_url)
+  );
+
+  // if no provider is found, return a 404
+  if (!provider) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  // find the endpoint that matches the url
+  const endpoint = provider.endpoints?.[0]?.url;
+
+  // if no endpoint is found, return a 404
+  if (!endpoint) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  // fetch the oembed data
+  const oembed = await fetch(
+    `${endpoint}?format=json&url=${params.url}&maxwidth=${params.maxwidth}&maxheight=${params.maxheight}`
+  );
+
+  // return the oembed data
+  return new Response(await oembed.text(), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
