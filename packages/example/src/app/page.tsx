0d2ed93994dbef59d48185a8134f07914cef9e43
Sarim Abbas <sarim.stays@gmail.com>
2023-05-04 02:34:32 -0700

    wip

--- packages/example/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Cardboard } from "@sarim.garden/cardboard";
-
-export default function Home() {
-  return (
-    <main>
-      <Cardboard url="https://twitter.com/jack/status/20" />
-    </main>
-  );
-}
