ae66960adbd863acaee3f6c511ba9e09ff3e16d4
Sarim Abbas <sarim.stays@gmail.com>
2023-05-04 02:24:37 -0700

    add banner

--- packages/publish/tsup.config.ts
+++ packages/publish/tsup.config.ts
@@ -4,4 +4,9 @@ export default defineConfig({
   entryPoints: ["src/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
+  esbuildOptions: (options) => {
+    options.banner = {
+      js: '"use client";',
+    };
+  },
 });
