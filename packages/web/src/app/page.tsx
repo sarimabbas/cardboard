e88deb923ab4357d10379308ab3b7d8222538fb4
Sarim Abbas <sarim.stays@gmail.com>
2023-05-05 00:54:26 -0700

    multiple

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -5,16 +5,32 @@ import { useState } from "react";
 
 export default function Home() {
   const [url, setUrl] = useState<string>("");
+  const [urls, setUrls] = useState<string[]>([]);
 
   return (
     <div className="flex flex-col justify-center gap-8">
-      <input
-        type="url"
-        className="p-4 border rounded-md"
-        placeholder="Your URL here"
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      {url && <Cardboard url={url} providerService="/api/v1" />}
+      <div className="flex items-center gap-4">
+        <input
+          type="url"
+          className="p-4 border rounded-md flex-1"
+          placeholder="Your URL here"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button
+          className="p-4 border rounded-md"
+          onClick={() => {
+            if (url === "") {
+              return;
+            }
+            setUrls([...urls, url]);
+          }}
+        >
+          Add
+        </button>
+      </div>
+      {urls.map((url, idx) => (
+        <Cardboard url={url} key={`${idx}=${url}`} />
+      ))}
     </div>
   );
 }
